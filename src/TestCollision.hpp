ef6163922fb16f0812f86dc12373b5057b216e87
justin <justin1726hernandez2@gmail.com>
2026-02-22 21:43:45 -0500

    Collision Detection implemented for Circles and added distance formula

--- src/TestCollision.hpp
+++ src/TestCollision.hpp
@@ -2,9 +2,3 @@
 #include "Collision.hpp"
 #include <glm/glm.hpp>
 
-
-CollisionPoints TestSphereSphere(const Collider* a, const Collider* b, const Transform* transformA, const Transform* transformB) {
-}
-
-CollisionPoints TestSphereSphere(const Collider* a, const Collider* b, const Transform* transformA, const Transform* transformB) {
-}
