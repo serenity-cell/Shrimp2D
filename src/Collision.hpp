31b5b867ce05a1afd9466e782441f144f39dff6e
justin <justin1726hernandez2@gmail.com>
2026-02-22 22:01:45 -0500

    Adds circle collision detection functions
    Introduces both unoptimized and optimized collision detection
    methods for circles, enabling distance-based intersection checks.
    Includes a utility for 2D Euclidean distance calculation.
    Improves flexibility for collision detection performance needs.

--- src/Collision.hpp
+++ src/Collision.hpp
@@ -20,3 +20,20 @@ struct PlaneCollider : Collider {
     vec2 Normal;
     float Distance; // distance from the origin
 };
+
+float Distance( vec2 a, vec2 b )
+{
+  return sqrt( (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) ); // Euclidean distance between two points 
+}
+bool CirclevsCircleUnoptimized( SphereCollider a, SphereCollider b )
+{
+  float r = a.Radius + b.Radius;
+  return r < Distance( a.Center, b.Center ); //checs if the distance between the cen
+}
+bool CirclevsCircleOptimized( SphereCollider a, SphereCollider b )
+{
+  float r = a.Radius + b.Radius;
+  r *= r;
+  return r < (a.Center.x - b.Center.x)*(a.Center.x - b.Center.x) + (a.Center.y - b.Center.y)*(a.Center.y - b.Center.y);
+}
+
