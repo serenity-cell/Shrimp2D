8ea0f573d7012da2e258385f39805353133c64a6
Just.PNG <justin1726hernandez2@gmail.com>
2026-02-23 18:32:53 -0500

    moved maht from header to cpp file

--- src/Collision.hpp
+++ src/Collision.hpp
@@ -1,7 +1,8 @@
 #include <glm/glm.hpp>
 #include "RigidBody.hpp"
 #include "PhysicsWorld.hpp"
-
+#ifndef COLLISION_HPP
+#define COLLISION_HPP
 enum ColliderType {
     CIRCLE,
     PLANE
@@ -21,27 +22,22 @@ struct PlaneCollider : Collider {
     float Distance; // distance from the origin
 };
 
-double Distance( vec2 a, vec2 b )
-{
-  return sqrt( (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) ); // Euclidean distance between two points 
-}
-bool CirclevsCircleUnoptimized( SphereCollider a, SphereCollider b )
-{
-  double r = a.Radius + b.Radius;
-  return r < Distance( a.Center, b.Center ); //checs if the distance between the cen
-}
-bool CirclevsCircleOptimized( SphereCollider a, SphereCollider b )
-{
-  double r = a.Radius + b.Radius;
-  r *= r;
-  return r < (a.Center.x - b.Center.x)*(a.Center.x - b.Center.x) + (a.Center.y - b.Center.y)*(a.Center.y - b.Center.y);
-}
+double Distance( vec2 a, vec2 b ){};
+bool CirclevsCircleUnoptimized( SphereCollider a, SphereCollider b ) {
+    double RadiusSum
+};
+bool CirclevsCircleOptimized( SphereCollider a, SphereCollider b ){
+    double RadiusSum
+};
+double RelativeVelocity( vec2 vA, vec2 vB vec2 n){
+    double VelocitySum
+};
 
-
-//V^{AB} = V^B - V^A 
+double DotProduct (vec2 a, vec2 b);
 
 // V^{AB} \cdot n = (V^B - V^A)
 
 //V_1 = \begin{bmatrix}x_1 \\y_1\end{bmatrix}, V_2 = \begin{bmatrix}x_2 \\y_2\end{bmatrix} \\ V_1 \cdot V_2 = x_1 * x_2 + y_2 * y_2 
 
-//
+
+#endif
