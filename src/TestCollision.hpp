71a406a5d9a7372fc8e2d1740b481098f5d7c572
justin <justin1726hernandez2@gmail.com>
2026-02-22 21:58:14 -0500

    Revert "Collision Detection implemented for Circles and added distance formula"
    This reverts commit ef6163922fb16f0812f86dc12373b5057b216e87.

--- src/TestCollision.hpp
+++ src/TestCollision.hpp
@@ -2,3 +2,9 @@
 #include "Collision.hpp"
 #include <glm/glm.hpp>
 
+
+CollisionPoints TestSphereSphere(const Collider* a, const Collider* b, const Transform* transformA, const Transform* transformB) {
+}
+
+CollisionPoints TestSphereSphere(const Collider* a, const Collider* b, const Transform* transformA, const Transform* transformB) {
+}
