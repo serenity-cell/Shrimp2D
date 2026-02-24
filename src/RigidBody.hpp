a4be63744423655b459b8531f0a838b82d7dd094
justin <justin1726hernandez2@gmail.com>
2026-02-23 21:15:07 -0500

    REDO starting over from beginning

--- src/RigidBody.hpp
+++
@@ -1,9 +0,0 @@
-#include <glm/glm.hpp>
-using namespace glm;
-
-struct RigidBody {
-    vec2 Position; // (x, y)
-    vec2 Velocity; // (x.velocity, y.velocity)
-    vec2 Force;
-    double mass
-};
