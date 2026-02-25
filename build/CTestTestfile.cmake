d51b4e5b9b8e3ea4483471c7d21b318e58f5cfc7
justin <justin1726hernandez2@gmail.com>
2026-02-24 20:16:19 -0500

    unmaking cmake build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justin/Desktop/Personal-Projects/Shrimp2D
-# Build directory: /home/justin/Desktop/Personal-Projects/Shrimp2D/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
