458095f87ac12687c85abb801d92efad950772b8
justin <justin1726hernandez2@gmail.com>
2026-03-06 14:38:23 -0500

    deleting unecessary comments for better ones

--- src/physicsWorld.cpp
+++ src/physicsWorld.cpp
@@ -4,18 +4,17 @@
 
 void physicsWorld::run()
 {   
-    totalCircles = 3; // quantitiy of circles to render
+    totalCircles = 6; // quantitiy of circles to render
     this->initWindow();
     this->initCircles();
+
+    // main loop for every updating factor
     while (this->window->isOpen())
     {
-        // update deltaTime
         this->updateDeltaTime();
 
-        // update SFML events
         this->updateSFMLEvents();
 
-        // update the current state
         this->update();
 
         this->updatePhysics();
@@ -27,6 +26,7 @@ void physicsWorld::run()
     }
 }
 
+//updates any input made by the user
 void physicsWorld::updateSFMLEvents()
 {
     while (this->window->pollEvent(sfEvent))
@@ -53,7 +53,7 @@ void physicsWorld::initWindow()
 this->window = new sf::RenderWindow(sf::VideoMode({maxWidth, maxHeight}), "Shrimp2D");
 }
 
-// renders the shapes onto the window
+// renders the bodies onto the window
 void physicsWorld::render()
 {  
     // renders multiple circleDrawn
@@ -64,6 +64,7 @@ void physicsWorld::render()
     };
 }
 
+// gives each individual body its own physics
 void physicsWorld::updatePhysics()
 {
     // resolving the physics of every individuL 
@@ -84,20 +85,22 @@ void physicsWorld::updatePhysics()
     }
 }
 
+
+// initializes all of the bodies positions to a certain location
 void physicsWorld::initCircles()
 {
     // renders multiple circleDrawn
     circleDrawn.resize(totalCircles);
     circleDrawnPosition.resize(totalCircles);
 
-    for (int i = 1; i < totalCircles; i++) 
+    for (int i = 0; i < totalCircles; i++) 
     {
-        circleDrawnPosition[i].setPosition(glm::vec2(i * 80.f + 30.f, i * 80.f + 50.f ));
-        circleDrawn[i].setPosition (i * 80.f + 30.f, i * 80 + 50);
+        circleDrawnPosition[i].setPosition(glm::vec2(i * 80.f + 50.f, i * 80.f + 50.f ));
+        circleDrawn[i].setPosition (i * 80.f + 50.f, i * 80 + 50);
     }
 }
 
 //updates all outside functions
 void physicsWorld::update()
-{   
+{  
 }
