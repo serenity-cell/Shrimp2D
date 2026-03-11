a49d528a98da58c851883817af4a42f905a57c44
justin <justin1726hernandez2@gmail.com>
2026-03-10 20:33:18 -0400

    added foundations for penetration correction, and cleaned up code

--- src/collision.hpp
+++ src/collision.hpp
@@ -10,39 +10,43 @@ private:
   // implements gravity upon the rigidBody
   glm::vec2 gravity = glm::vec2(0.0f, 98.1f);
 
+  // penetration correction variables
+  glm::vec2 difference;
+  float distance;
+
   // declerations for resolveCollision function
   glm::vec2 normal;
   glm::vec2 relVel;
   float normVel;
   float j;
 
+  // constants for penetration
+  float slop;
+  float percent;
+  
 public:
+
   void applyGravity(circleBody &body);
 
+  // collision correction onto the borders of the window
   void resolveGround(circleBody &body, float groundY);
-  
   void resolveWall(circleBody &body, float groundX);
-  // math formulas used for checking collision
+
+  // collision formulas
   float dotProduct(const glm::vec2 &objectA, const glm::vec2 &objectB);
-
   float radiiSum(const float radiusA, const float radiusB);
-
   glm::vec2 collisionNormal(glm::vec2 objectA, glm::vec2 objectB);
-
   glm::vec2 relativeVelocity(glm::vec2 velocityA, glm::vec2 velocityB);
-
   float normalVelocity(glm::vec2 relativeVelocity, glm::vec2 normal);
 
-  void penetrationCorrection(const glm::vec2 bodyA, const glm::vec2 bodyB, float radiusA, float radiusB);  
+  // formulas for penetration upon a circle collision
+  float invSum (float invMassA, float invMassB);
+  void penetrationDepth(const glm::vec2 bodyA, const glm::vec2 bodyB, float radiusA, float radiusB);
+  void penetrationCorrection(const glm::vec2 bodyA, const glm::vec2 bodyB, float radiusA, float radiusB); 
 
-  // gets the checks collision and if true, acts upon it accordingly
-  bool isColliding(const glm::vec2 &objectA, const glm::vec2 &objectB,
-                   const float radiusA, const float radiusB);
-
-
-
+  // collision checks
+  bool isColliding(const glm::vec2 &objectA, const glm::vec2 &objectB, const float radiusA, const float radiusB);
   float impulse(float normalVelocity, float epsilon, float massA, float massB);
-
   void resolveCollision(circleBody &bodyA, circleBody &bodyB);
 };
 
